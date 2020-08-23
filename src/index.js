8b460f76828a78c4e31320c99212593b1a91cf87
bjupreti <bjupreti123@gmail.com>
2020-08-23 11:44:33 +0545

    Added enviroment variabled in app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <p>Hello Bijay I'm {process.env.NODE_ENV} environment.</p>
+      <p>Secret Key: {process.env.REACT_APP_SECRET_KEY}</p>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
